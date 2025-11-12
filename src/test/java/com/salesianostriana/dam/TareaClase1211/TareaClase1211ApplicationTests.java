55ee828fc137a0ee1ca0a267e485d4b8d229fcc5
Gabriel-Perez-Marquez <gabrielperezmarquez18@gmail.com>
2025-11-12 09:35:01 +0100

    gi

---
+++ src/test/java/com/salesianostriana/dam/TareaClase1211/TareaClase1211ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.TareaClase1211;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareaClase1211ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
